a2bcc2c0a3c7fd093772f3babee6976c8b20179f
Christoph G. Vieth <christoph@vieth.me>
2017-12-23 14:26:47 +0100

    Implemented sendMessage Method

---
+++ brematic.js
@@ -0,0 +1,38 @@
+'use strict';
+/**
+ * Initialize Brematic
+ * @param {Object} config Configuration
+ */
+var Brematic = function(config) {};
+
+/**
+ * Send a Message to Device
+ * @private
+ * @param {String} message Message to be send
+ */
+Brematic.prototype.sendMessage = function(message) {
+  return new Promise((resolve, reject) => {
+    var dgram = require('dgram');
+    var buffer = new Buffer(message);
+
+    var client = dgram.createSocket('udp4');
+    client.send(
+      buffer,
+      0,
+      buffer.length,
+      this.port,
+      this.host,
+      (err, bytes) => {
+        // Close connection
+        client.close();
+
+        if (err) {
+          reject(err);
+        } else {
+          resolve(bytes);
+        }
+      });
+  });
+};
+
+module.exports = Brematic;
